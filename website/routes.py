d17a834c2597518c7bb165e3fef3d3122ffb510d
Khang Tran <19110375@student.hcmute.edu.vn>
2021-11-27 18:22:49 +0700

    Đăng kí/Đăng nhập/Phân Quyền/Ngăn người dùng điều chỉnh URL

--- website/routes.py
+++ website/routes.py
@@ -3,8 +3,8 @@ import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
-
-from flask import Blueprint, render_template, request, redirect, url_for
+from functools import wraps
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from website import app, db, utils
 from website.models import BenhNhan, DanhSachKham, TaiKhoan, PhieuKham, ToaThuoc, Thuoc, chitiettoathuoc, HoaDon
 
@@ -14,13 +14,83 @@ user = Blueprint('user', __name__)
 bacsi = Blueprint('bacsi', __name__)
 admin = Blueprint('admin', __name__)
 
+
 # Xử lí các trang
 #--------------------NGƯỜI DÙNG---------------------
+# Chặn người dùng đăng nhập trái phép
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('user.user_login'))
+
+    return wrap
 
 
 @user.route('/')
+@login_required
 def user_home():
     return render_template('home.html')
+
+
+@user.route('/login', methods=['GET', 'POST'])
+def user_login():
+    if request.method == 'POST':
+        session.pop('user', None)
+        tentaikhoan = request.form['username']
+        matkhau = request.form['password']
+        dangnhap = utils.get_login(tentaikhoan, matkhau)
+        if dangnhap == None:
+            flash("Tên đăng nhập hoặc mật khẩu không đúng", "error")
+        else:
+            #Tạo session
+            session["maquyen"] = dangnhap.maquyen
+            session["logged_in"] = True
+            #Phân quyền với 0 - Admin, 1 - Bác sĩ, 2 - Y tá, 3 - Người dùng
+            if (dangnhap.maquyen == 0):
+                return redirect(url_for("admin.home"))
+            if (dangnhap.maquyen == 1):
+                return redirect(url_for("bacsi.home"))
+            if (dangnhap.maquyen == 2):
+                return redirect(url_for("yta.yta_home"))
+            if (dangnhap.maquyen == 3):
+                return redirect(url_for("user.user_home"))
+    return render_template('login.html')
+
+
+@user.route('/logout')
+@login_required
+def user_logout():
+    session.clear()
+    return redirect(url_for('user.user_login'))
+
+
+@user.route('/register', methods=['GET', 'POST'])
+def user_register():
+    if request.method == 'POST':
+        session.pop('user', None)
+        tentaikhoan = request.form['username']
+        matkhau = request.form['password']
+        matkhau_nhaplai = request.form['repassword']
+        print(tentaikhoan)
+        print(matkhau_nhaplai)
+        print(matkhau)
+        dangnhap = utils.get_taikhoan_by_usernames(tentaikhoan)
+        if (dangnhap is None):
+            if (matkhau == matkhau_nhaplai):
+                taiKhoanMoi = TaiKhoan(tendangnhap=tentaikhoan,
+                                       matkhau=matkhau,
+                                       maquyen=3)
+                db.session.add(taiKhoanMoi)
+                db.session.commit()
+                flash("Tạo tài khoản thành công", "success")
+            else:
+                flash("Mật khẩu nhập lại phải trùng khớp", "error")
+        else:
+            flash("Tài khoản đã tồn tại", "error")
+    return render_template('register.html')
 
 
 #--------------------Y TÁ---------------------
@@ -28,19 +98,49 @@ def user_home():
 
 #Homepage
 @yta.route('/home')
+@login_required
 def yta_home():
     return render_template('yta.html')
 
 
 #Lập hoá đơn
-@yta.route('/laphoadon')
+@yta.route('/laphoadon', methods=['GET', 'POST'])
+@login_required
 def yta_laphoadon():
+    maphieukham = request.form.get('maphieukham')
+    matoa = request.form.get('matoathuoc')
+    ngayban = utils.today
+    dathanhtoan = 0
+
+    try:
+        phieukham_obj = utils.get_pk_by_mapk(maphieukham)
+        toathuoc_obj = utils.get_toathuoc_by_matoa(matoa)
+        # Tạo ra một hoá đơn có mã phiếu khám và mã toa
+        # Xử lí tính tổng thu qua phiếu khám và mã toa
+        if (phieukham_obj != None and toathuoc_obj != None):
+            new_hoadon = HoaDon(maphieukham=maphieukham,
+                                ngayban=ngayban,
+                                o_phieukham=phieukham_obj,
+                                o_toathuoc=toathuoc_obj,
+                                tongthu=phieukham_obj.tienkham +
+                                toathuoc_obj.tienthuoc,
+                                dathanhtoan=False)
+            db.session.add(new_hoadon)
+            db.session.commit()
+            print("Thêm thành công")
+        else:
+            print("Không lấy được đối tượng")
+        return redirect(url_for("yta.yta_laphoadon"))
+
+    except:
+        print("Thêm thất bại")
     return render_template('laphoadon.html')
 
 
 #CRUD Danh sách khám
 #Thêm bệnh nhân khám
 @yta.route('/themdskham', methods=['POST', 'GET'])
+@login_required
 def yta_themdskham():
     ngaykham = utils.today
     hoten = request.form.get("hoten")
@@ -71,6 +171,7 @@ def yta_themdskham():
 
 # Xoá bệnh nhân trong danh sách khám
 @yta.route('/xoa-danhsachkham/<int:id>', methods=['GET', 'POST'])
+@login_required
 def yta_xoadanhsachkham(id):
     id = str(id)
     nguoikham = DanhSachKham.query.filter(DanhSachKham.mads == id).first()
@@ -89,6 +190,7 @@ def yta_xoadanhsachkham(id):
 
 # Danh sách bệnh nhân khám ngày hôm nay
 @yta.route('/danhsachkham')
+@login_required
 def yta_danhsachkham():
     danhsachkham = utils.get_ds_all()
     return render_template('danhsachkham.html', danhsach=danhsachkham)
@@ -96,8 +198,10 @@ def yta_danhsachkham():
 
 # Báo cáo
 @yta.route('/baocao')
+@login_required
 def yta_baocao():
-    return render_template('baocao.html')
+    danhsachbc = utils.get_baocao()
+    return render_template('baocao.html', danhsach=danhsachbc)
 
 
 #Thêm người
--- website/utils.py
+++ website/utils.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from website.models import BenhNhan, DanhSachKham, TaiKhoan, PhieuKham, ToaThuoc, Thuoc, chitiettoathuoc, HoaDon
 from datetime import datetime, timedelta
+from sqlalchemy.sql import func
 
 #Xử lí một số vấn đề về thời gian
 # Lấy thời gian ngày hôm nay [ngày và giờ hiện tại]
@@ -21,21 +22,50 @@ tomorrow = today + timedelta(days=1)
 
 # Xử lí các chức năng CRUD
 # --------------------TÀI KHOẢN---------------------
-def get_taikhoan():
+def get_taikhoan_all():
     return TaiKhoan.query.all()
+
+
+def get_login(username, password):
+    return TaiKhoan.query.filter(TaiKhoan.tendangnhap == username,
+                                 TaiKhoan.matkhau == password).first()
+
+
+def get_taikhoan_by_usernames(usernames):
+    return TaiKhoan.query.filter(TaiKhoan.tendangnhap == usernames).first()
 
 
 # --------------------BỆNH NHÂN---------------------
 
+
 # --------------------PHIẾU KHÁM--------------------
+def get_pk_by_mapk(mapk):
+    return PhieuKham.query.filter(PhieuKham.mapk == mapk).first()
+
 
 # --------------------TOA THUỐC---------------------
+def get_toathuoc_by_matoa(matoa):
+    return ToaThuoc.query.filter(ToaThuoc.matoa == matoa).first()
+
 
 # ----------------CHI TIẾT TOA THUỐC----------------
 
 # ---------------------THUỐC------------------------
 
+
 # ---------------------HOÁ ĐƠN----------------------
+# Lấy hoá đơn chưa thanh toán hiện lên
+def get_hoadon():
+    return HoaDon.query.filter(HoaDon.dathanhtoan == 0).all()
+
+
+# Lấy báo cáo doanh thu
+def get_baocao():
+    return HoaDon.query.with_entities(
+        HoaDon.ngayban,
+        func.sum(HoaDon.ma_pk).label("tong_bn"),
+        func.sum(HoaDon.tongthu).label("doanh_thu")).order_by(
+            HoaDon.ngayban).all()
 
 
 # -----------------DANH SÁCH KHÁM-------------------
@@ -49,4 +79,4 @@ def get_ds_today():
 
 
 if __name__ == '__main__':
-    print(get_ds_today())
+    print(get_login(1, 2))
